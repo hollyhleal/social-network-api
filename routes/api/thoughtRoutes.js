06288786c8c657a44ead126b9a9bf67358e5c53a
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-05 17:20:29 -0600

    update thoughtRoutes with all paths

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,8 +1,28 @@
 const router = require("express").Router();
 // require controller
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
 
 // /api/thoughts
+router
+  .route("/")
+  .get(getThoughts)
+  .get(getSingleThought)
+  .post(createThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
+router
+  .route("/:thoughtId/reactions")
+  .post(createReaction)
+  .delete(deleteReaction);
 
 module.exports = router;
