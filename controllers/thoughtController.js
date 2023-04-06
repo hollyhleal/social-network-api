2f765632e0893d3bd87568354ff0eefee3f8f12d
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-05 21:41:02 -0600

    add routes for getThoughts, getSingleThought, createThought, updateThought and deleteThought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,10 +3,64 @@ const { User, Thought } = require("../models");
 
 module.exports = {
   // GET all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+
   // GET a single thought by its _id
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res
+              .status(404)
+              .json({ message: "No thought exists under that id." })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
   // POST a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err))
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "Thought created, but user not found.",
+            })
+          : res.json({ message: "Thought added to user's thoughts." })
+      );
+  },
+
   // PUT to update a thought by its _id
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought exists with this id." })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
   // DELETE to remove a thought by its _id
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought exists with this id." })
+          : res.json({ message: "Thought has been deleted." })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
   // POST to create a reaction stored in a single thought's reactions array field
   // DELETE to pull and remove a reaction by the reaction's reactionId value
 };
