9b248a97e8f0058f4e86acc2977d5a6b716cff1e
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-05 22:08:03 -0600

    moved reactionSchema above thoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,30 @@
 const { Schema, Types } = require("mongoose");
 
+// Schema named "reactionSchema" for subdocument
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    // Use a getter method to format the timestamp on the query
+    get: (date) => {
+      if (date) return date.toISOString().split("T")[0];
+    },
+  },
+});
+
 // Schema to create Thought model
 const thoughtSchema = new Schema({
   thoughtText: {
@@ -26,31 +51,6 @@ const thoughtSchema = new Schema({
 // Virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
-});
-
-// Schema named "reactionSchema" for subdocument
-const reactionSchema = new mongoose.Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxLength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    // Use a getter method to format the timestamp on the query
-    get: (date) => {
-      if (date) return date.toISOString().split("T")[0];
-    },
-  },
 });
 
 module.exports = thoughtSchema;
