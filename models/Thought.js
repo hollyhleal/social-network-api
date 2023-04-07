aff10927a0ed5c2c20a6908261a7181bd1f5f9ef
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-06 21:31:16 -0600

    update to require in model from mongoose and add export for thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const { Schema, Types } = require("mongoose");
+const { Schema, Types, model } = require("mongoose");
 
 // Schema named "reactionSchema" for subdocument
 const reactionSchema = new Schema({
@@ -26,31 +26,41 @@ const reactionSchema = new Schema({
 });
 
 // Schema to create Thought model
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    // Use a getter method to format the timestamp on the query
-    get: (date) => {
-      if (date) return date.toISOString().split("T")[0];
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
     },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // Use a getter method to format the timestamp on the query
+      get: (date) => {
+        if (date) return date.toISOString().split("T")[0];
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 // Virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
-module.exports = thoughtSchema;
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
