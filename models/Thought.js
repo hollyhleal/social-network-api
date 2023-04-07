45c1bbdf458193163b86182d8a2c39bdac386a73
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-07 16:47:44 -0600

    add moment js to format date

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,37 @@
 const { Schema, Types, model } = require("mongoose");
+const moment = require("moment");
 
 // Schema named "reactionSchema" for subdocument
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxLength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    // Use a getter method to format the timestamp on the query
-    get: (date) => {
-      date.toDateString();
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter method to format the timestamp on the query
+      get: (currentDate) => moment(currentDate).format("MMM DD, YYYY hh:mm a"),
     },
   },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // Schema to create Thought model
 const thoughtSchema = new Schema(
@@ -36,11 +44,9 @@ const thoughtSchema = new Schema(
     },
     createdAt: {
       type: Date,
-      default: Date.now(),
+      default: Date.now,
       // Use a getter method to format the timestamp on the query
-      get: (date) => {
-        date.toDateString();
-      },
+      get: (currentDate) => moment(currentDate).format("MMM DD, YYYY hh:mm a"),
     },
     username: {
       type: String,
@@ -51,6 +57,7 @@ const thoughtSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
