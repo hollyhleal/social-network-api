ea2131984e429cad732c7ec293717bcf31203753
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-07 17:26:07 -0600

    update to createThought route

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -24,7 +24,7 @@ module.exports = {
       .then((thought) => {
         return User.findOneAndUpdate(
           { _id: req.body.userId },
-          { $addtoSet: { thoughts: thought._id } },
+          { $addtoSet: { thoughts: req.params.thoughtId } },
           { new: true }
         );
       })
